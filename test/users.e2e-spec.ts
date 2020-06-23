1d2db13562d54001c9491a55c7bdb947a5374470
katsanva <this.is.ftm@gmail.com>
2020-06-23 22:19:42 +0300

    tests: success path on get user

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -9,6 +9,7 @@ import { SwaggerValidatorPipe } from '../src/common/swagger-validator.pipe';
 import { USERS, USER } from '../src/routes';
 
 import { CreateUserDto } from '../src/users/dto/create-user.dto';
+import { UserDto } from 'src/users/dto/user.dto';
 
 describe('UsersController (e2e)', () => {
   let app: INestApplication;
@@ -82,11 +83,33 @@ describe('UsersController (e2e)', () => {
 
   describe(`${USER} (GET)`, () => {
     const constructRoute = (id: string) => USER.replace(':id', id);
+    let user: UserDto;
+
+    beforeAll(async () => {
+      const payload: CreateUserDto = {
+        email: faker.internet.email(),
+        password: faker.random.alphaNumeric(7) + 'D',
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+      };
+
+      const res = await request(app.getHttpServer())
+        .post(USERS)
+        .send(payload);
+
+      user = res.body;
+    });
 
     it('should return 404 for random id', () => {
       return request(app.getHttpServer())
         .get(constructRoute(faker.random.alphaNumeric(5)))
         .expect(HttpStatus.NOT_FOUND);
+    });
+
+    it('should return 200 on valid id', () => {
+      return request(app.getHttpServer())
+        .get(constructRoute(user._id))
+        .expect(HttpStatus.OK, user);
     });
   });
 });
